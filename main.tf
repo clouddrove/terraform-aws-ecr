1a6ebab7579eedc48799a1f325781234135fbac1
himanshu-uniyal <himanshu@clouddrove.com>
2020-10-13 14:29:11 +0530

    pipeline added

--- main.tf
+++ main.tf
@@ -28,10 +28,10 @@ resource "aws_ecr_repository" "default" {
   count                = var.enabled_ecr ? 1 : 0
   name                 = module.labels.id
   tags                 = module.labels.tags
-  image_tag_mutability = "MUTABLE"
+  image_tag_mutability = var.image_tag_mutability
 
   image_scanning_configuration {
-    scan_on_push = true
+    scan_on_push = var.scan_on_push
   }
 
 }
