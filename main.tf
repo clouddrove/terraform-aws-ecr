02550e172520973362c2cc0afeda741b90d8e78d
yadavprakash <yadavprakash.it@gmail.com>
2021-06-16 20:23:27 +0530

    update in 0.15

--- main.tf
+++ main.tf
@@ -13,10 +13,11 @@ locals {
 #              tags for resources. You can use terraform-labels to implement a strict
 #              naming convention.
 module "labels" {
-  source = "git::https://github.com/clouddrove/terraform-labels.git?ref=tags/0.14.0"
-
+  source  = "clouddrove/labels/aws"
+  version = "0.15.0"
 
   name        = var.name
+  repository  = var.repository
   environment = var.environment
   managedby   = var.managedby
   label_order = var.label_order
--- variables.tf
+++ variables.tf
@@ -78,3 +78,9 @@ variable "scan_on_push" {
   description = "Indicates whether images are scanned after being pushed to the repository (true) or not scanned (false)."
   default     = true
 }
+
+variable "repository" {
+  type = string
+  default = ""
+  description = "Terraform current module repo"
+}
